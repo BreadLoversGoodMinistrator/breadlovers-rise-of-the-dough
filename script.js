fbdc6b1020a32d5c623566982065e81810ae2989
BreadLoversGoodMinistrator <2953@lfp.edu.co>
2026-02-14 13:27:02 -0500

    Update and rename mi-web-firebase/script.js to script.js
    again..... ahhhhhh......

---
+++ script.js
@@ -0,0 +1,20 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHQVIX_P94pKOeE1-TsiVhCzM5pM1AaRU",
+  authDomain: "breadloverswebindies.firebaseapp.com",
+  databaseURL: "https://breadloverswebindies-default-rtdb.firebaseio.com",
+  projectId: "breadloverswebindies",
+  storageBucket: "breadloverswebindies.firebasestorage.app",
+  messagingSenderId: "495702964488",
+  appId: "1:495702964488:web:7ad74438e17d1ee057d4bd",
+  measurementId: "G-E0GYLJ7K1H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+firebase.database().ref("ranking").push({
+  name: playerName,
+  score: playerScore
+});
