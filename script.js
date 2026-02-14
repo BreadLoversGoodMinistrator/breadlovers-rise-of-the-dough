e68df524cb176d766e0542c22713403af0cf89ac
BreadLoversGoodMinistrator <2953@lfp.edu.co>
2026-02-14 14:23:01 -0500

    Update script.js
    .......GAAAAAH!!!!!

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-import  { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHQVIX_P94pKOeE1-TsiVhCzM5pM1AaRU",
+  apiKey: "TU_API_KEY",
   authDomain: "breadloverswebindies.firebaseapp.com",
   databaseURL: "https://breadloverswebindies-default-rtdb.firebaseio.com",
   projectId: "breadloverswebindies",
   storageBucket: "breadloverswebindies.firebasestorage.app",
   messagingSenderId: "495702964488",
-  appId: "1:495702964488:web:7ad74438e17d1ee057d4bd",
-  measurementId: "G-E0GYLJ7K1H"
+  appId: "1:495702964488:web:7ad74438e17d1ee057d4bd"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
-//PRUEBA: enviar dato de ejemplo al ranking
+// PRUEBA
 push(ref(db, "ranking"), {
   name: "JugadorPrueba",
   score: Math.floor(Math.random() * 100)
 });
 
-console.log("🔥 Firebase conectado correctamente ")
+console.log("🔥 Firebase Database funcionando");
